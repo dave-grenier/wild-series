0286280a48621945422c4d68c4e538fe61006ab2
dave-grenier <grenierdave@hotmail.com>
2020-06-22 21:05:51 +0200

    controller default et wild

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/WildController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+Class DefaultController extends AbstractController
+{
+   /**
+     * @Route("/default", name="app_index")
+    */
+    public function index() :Response
+    {
+        return $this->render('default/index.html.twig', [
+                'bienvenue' => 'Bienvenue !',
+        ]);
+    }
+}    
